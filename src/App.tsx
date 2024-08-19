4ac4836bae55cc753a89b243b4e0775a9a7dbd95
dungle <quangdung100194@gmail.com>
2024-08-19 21:03:34 +0700

    fix: store connection return strategy

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
 import {Footer} from "./components/Footer/Footer";
 import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    console.log("-------returnStrategy none")
+    const [returnStrategy, setReturnStrategy] = useState("back")
     return (
       <TonConnectUIProvider
           manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
@@ -27,13 +27,31 @@ function App() {
           }}
           actionsConfiguration={{
             //   twaReturnUrl: 'https://t.me/mpc_wallet_connect_bot/tobi_wallet',
-            returnStrategy: 'https://www.google.com.vn/'
+            returnStrategy: returnStrategy as any
             
 
           }}
       >
         <div className="app">
             <Header />
+            <div style={{
+              color: '#fff',
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+             <p style={{
+              fontSize: '20px'
+             }}> Connect Return Strategy</p>
+             <input value={returnStrategy} onChange={e => setReturnStrategy(e.target.value)} style={{
+              height: 48,
+              fontSize: 18,
+              paddingLeft: 10,
+              marginBottom: 12
+             }} placeholder='input return strategy'/>
+             <strong>Note: There are 3 values valid for input: back, none, link (ex: "https://www.google.com.vn/")</strong>
+              </div>
             <TxForm />
             <TonProofDemo />
             <Footer />
