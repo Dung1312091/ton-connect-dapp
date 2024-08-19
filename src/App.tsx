9e8e83032dda34c5182efe43a66519612c5d91d5
dungle <quangdung100194@gmail.com>
2024-08-19 18:53:38 +0700

    feat bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
 import {useEffect} from "react";
 
 function App() {
-    console.log("-------4")
+    console.log("-------5")
     return (
       <TonConnectUIProvider
           manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
