31fcfaaf1b48bc20c5c9d523c871b25f92cee467
dungle <quangdung100194@gmail.com>
2024-08-19 19:44:46 +0700

    feat bug

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                       name: "TobiWallet",
                       imageUrl: "https://wallet.tg/images/logo-288.png",
                       aboutUrl: "https://wallet.tg/",
-                      universalLink: "https://t.me/telifidev_bot/wallet",
+                      universalLink: "https://t.me/telifidev_bot/local",
                       bridgeUrl: "https://bridge.tonapi.io/bridge",
                       platforms: ["ios", "android", "macos", "windows", "linux"]
                   }
